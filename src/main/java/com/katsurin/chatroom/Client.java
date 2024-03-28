5d398c62c3e32ea8d6910b308209d8b545d388e4
Frederick Katsura <fkatsura@katsurin.com>
2024-03-28 15:02:50 -0400

    Cleaned Up Code + Added Outputs to Tell Other Clients Who Has Connected/Disconnected

--- src/main/java/com/katsurin/chatroom/Client.java
+++ src/main/java/com/katsurin/chatroom/Client.java
@@ -1,40 +1,30 @@
 package com.katsurin.chatroom;
 
-import com.katsurin.chatroom.enums.ChatRoomEvents;
 import com.katsurin.chatroom.threads.*;
 
-import java.beans.*;
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client implements PropertyChangeListener {
+public class Client {
     private Socket socket = null;
-    private BufferedReader in = null;
     private PrintWriter out = null;
     private String username = "";
     private ClientSender clientRunner = null;
+    private Scanner scanner = new Scanner(System.in);
 
     public Client(Integer serverPort) {
+        if (username.isEmpty()) {
+            System.out.print("Enter your name: ");
+            updateUsername(scanner.nextLine());
+        }
+
         try {
             socket = new Socket("127.0.0.1", serverPort);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
             clientRunner = new ClientSender(socket);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public void propertyChange(PropertyChangeEvent evt) {
-        if (evt.getPropertyName().equals(ChatRoomEvents.DISCONNECTEDTHREAD.toString())) {
-            System.out.println("[System] You have disconnected from the chatroom");
-            try {
-                socket.close();
-            } catch (IOException e) {
-                System.out.println("Failed to close client out of server.");
-                e.printStackTrace();
-            }
         }
     }
 
@@ -43,30 +33,20 @@ public class Client implements PropertyChangeListener {
     }
 
     public void onStart() {
-        /*Sender senderThread = new Sender(out, "Client");
-        senderThread.start();
-
-        Receiver receiver = new Receiver(in, out);
-        receiver.startThread(this);*/
-
         try {
-            Scanner scanner = new Scanner(System.in);
             String userInput;
 
             new Thread(clientRunner).start();
             System.out.println("[System] Connection established");
-
-            if (username.isEmpty()) {
-                System.out.print("Enter your name ");
-                updateUsername(scanner.nextLine());
-            }
+            out.println("[System] " + username + " has connected to the room!");
 
             do {
                 userInput = scanner.nextLine();
-                out.println("[" + username + "]" + ": " + userInput);
                 if (userInput.equals("exit")) {
+                    out.println("[System] " + username + " has disconnected from the room...");
                     break;
                 }
+                out.println("[" + username + "]" + ": " + userInput);
             } while (!userInput.equals("exit"));
         } catch (Exception e) {
             System.out.println("Exception occurred in client main: " + e.getStackTrace());
--- src/main/java/com/katsurin/chatroom/Server.java
+++ src/main/java/com/katsurin/chatroom/Server.java
@@ -1,13 +1,11 @@
 package com.katsurin.chatroom;
 
-import com.katsurin.chatroom.enums.ChatRoomEvents;
 import com.katsurin.chatroom.threads.*;
-import java.beans.*;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
-public class Server implements PropertyChangeListener {
+public class Server {
     private ServerSocket serverSocket = null;
     private Socket clientSocket = null;
     private BufferedReader in = null;
@@ -22,26 +20,6 @@ public class Server implements PropertyChangeListener {
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public void onStart() {
-        Sender senderThread = new Sender(out, "Server");
-        senderThread.start();
-
-        Receiver receiver = new Receiver(in, out);
-        receiver.startThread(this);
-    }
-
-    public void propertyChange (PropertyChangeEvent evt) {
-        if (evt.getPropertyName().equals(ChatRoomEvents.DISCONNECTEDTHREAD.toString())) {
-            try {
-                clientSocket.close();
-                serverSocket.close();
-            } catch (IOException e) {
-                System.out.println("Server failed to close connections");
-                e.printStackTrace();
-            }
         }
     }
 
